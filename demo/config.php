5c555e67da6415203b567af08fc5e307e5e837e8
Garming <garming@msn.com>
2017-01-11 20:52:04 +0800

    support different database

--- demo/config.php
+++ demo/config.php
@@ -15,13 +15,13 @@ return [
         'database' => 'test_db_1',
         'charset' => 'utf8'
     ],
-//    'no_def' => [// if you have tow more different db,you can add like this
-//        'driver' => 'mysql',
-//        'hostname' => '127.0.0.1',
-//        'port' => '3306',
-//        'username' => 'root',
-//        'password' => '',
-//        'database' => 'test_db_2',
-//        'charset' => 'utf8'
-//    ]
+    'no_def' => [// if you have tow more different db,you can add like this
+        'driver' => 'mysql',
+        'hostname' => '127.0.0.1',
+        'port' => '3306',
+        'username' => 'root',
+        'password' => '',
+        'database' => 'test_db_2',
+        'charset' => 'utf8'
+    ]
 ];
