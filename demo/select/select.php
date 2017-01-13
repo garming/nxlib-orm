85d93224832c6460a36ecbcfbff6951663caa485
Garming <garming@msn.com>
2017-01-13 16:27:25 +0800

    add select demo

--- demo/select/select.php
+++ demo/select/select.php
@@ -5,3 +5,9 @@
  * Date: 2017/1/4
  * Time: 18:05
  */
+include "../public.php";
+$config = include "../config.php";
+$init = \NxLib\RdsOrm\Instance::init($config);
+$default = \NxLib\RdsOrm\Lib\Mysql\Instance::get();
+
+$table = "users";
